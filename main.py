6187f56e6493ba272b3ebcd6dbb1734b2780e61f
Anki Satya <95831101+Ansaku@users.noreply.github.com>
2022-01-18 02:23:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ while True:
         @bot.message_handler(commands=['start'])
         def start_message(message):
             bot.send_message(message.chat.id,
-                             'Hai Pengguna., Selamat datang di TempEmail Bot \nUsage:_\nUntuk Menghasilkan email klik tombol "Buat email"\nUntuk menyegarkan kotak masuk Anda, klik tombol "Refresh inbox". Setelah surat baru tiba, Anda akan melihat tombol dengan baris subjek, klik tombol read the message. \n\n Dev : @AnkiSatya',
+                             'Hai Pengguna., Selamat datang di TempEmail Bot \nPenggunaan:\nUntuk Menghasilkan email klik tombol "Buat email"\nUntuk menyegarkan kotak masuk Anda, klik tombol "Refresh inbox". Setelah surat baru tiba, Anda akan melihat tombol dengan baris subjek, klik tombol read the message. \n\n Dev : @AnkiSatya',
                              reply_markup=keyboard)
 
 
         @bot.message_handler(content_types=['text'])
         def send_text(message):
-            if message.text.lower() == 'generate email':
+            if message.text.lower() == 'buat email':
                 email = requests.get("https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1").json()[0]
                 ekeyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
                 ekeyboard.add(InlineKeyboardButton(text='Buat email'))
@@ -34,9 +34,9 @@ while True:
                 ekeyboard.add(InlineKeyboardButton(text='Tentang'))
                 bot.send_message(message.chat.id, "E-Mail Sementara Anda:")
                 bot.send_message(message.chat.id, str(email), reply_markup=ekeyboard)
-            elif message.text.lower() == 'refresh inbox':
+            elif message.text.lower() == 'refresh pesan':
                 bot.send_message(message.chat.id, 'Pertama, buat email anda', reply_markup=keyboard)
-            elif message.text.lower() == 'about':
+            elif message.text.lower() == 'tentang':
                 bot.send_message(message.chat.id,
                                  'Apa itu Email Semantara?\n- Itu adalah layanan email gratis yang memungkinkan untuk menerima email di alamat sementara yang akan dihancurkan sendiri setelah waktu tertentu berlalu. Itu juga dikenal dengan nama-nama seperti tempmail, 10minutemail, 10minmail, throwaway email, fake-mail , fake email generator, burner mail atau trash-mail\n\nBagaimana Email Sementara Menjadi Lebih Aman bagi Anda?\n- Menggunakan Email sementara memungkinkan Anda untuk sepenuhnya melindungi kotak surat asli Anda dari hilangnya informasi pribadi. Alamat email sementara Anda sepenuhnya anonim. Detail Anda: informasi tentang orang Anda dan pengguna yang berkomunikasi dengan Anda, alamat IP, alamat email dilindungi dan sepenuhnya dirahasiakan.\n\n➪ Bot Name : AnsakuMail Bot\n➪ Author : @AnkiSatya\n➪ Language : Python \n➪ Donate : https://saweria.co/ansaku')
             elif message.text.lower()[14] == "[":
@@ -77,9 +77,9 @@ while True:
                     msg = requests.get("https://www.1secmail.com/api/v1/?action=readMessage&login=" + email[:email.find(
                         "@")] + "&domain=" + email[email.find("@") + 1:] + "&id=" + id).json()
                     bot.send_message(message.chat.id,
-                                     'Pesan ✉️\n\n   Dari: ' + msg['from'] + "\n   Subjek: " + msg[
-                                         'subject'] + "\n   Tanggal: " + msg[
-                                         'date'] + "\n   Teks: " + msg['textBody'])
+                                     'Pesan ✉️\n\n   Dari: ' + msg['dari'] + "\n   Subjek: " + msg[
+                                         'subjek'] + "\n   Tanggal: " + msg[
+                                         'tanggal'] + "\n   Teks: " + msg['textBody'])
                 except BaseException:
                     pass
 
